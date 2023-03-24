8b4433a0afdd538d45c4ac392c8bded5a343bf79
sieun park <qkr990401@daum.net>
2023-03-24 15:51:18 +0900

    차기 접수일 현재날짜 전이면 빨강색

--- www/vdcs/document/latest/latest_view.php
+++ www/vdcs/document/latest/latest_view.php
@@ -17,6 +17,9 @@
 .resultNull {
     font-style: italic;
 }
+.beforeToday {
+    color: red;
+}
 </style>
 <script>
 var vm = new Vue({
@@ -157,48 +160,48 @@ var vm = new Vue({
                     $(this).html(removeTag);
                 });
                 //  검색 배경
-                if(data.isRebrowsing == false) {
-                    if(data.researchOption == "so_all" && data.researchSave) {
-                        $(".tblLatestList td").each(function() {
-                            var tdText = $(this).text().toUpperCase();
-                            var upperSearch = data.researchSave.toUpperCase();
-                            if(tdText.match(upperSearch)) {
-                                var indexOri = tdText.match(upperSearch).index;
-                                var lengthOri = upperSearch.length;
-                                var oriText = $(this).text().substr(indexOri, lengthOri);
-                                const redRegExp = new RegExp(data.researchSave, 'gi');
+                // if(data.isRebrowsing == false) {
+                //     if(data.researchOption == "so_all" && data.researchSave) {
+                //         $(".tblLatestList td").each(function() {
+                //             var tdText = $(this).text().toUpperCase();
+                //             var upperSearch = data.researchSave.toUpperCase();
+                //             if(tdText.match(upperSearch)) {
+                //                 var indexOri = tdText.match(upperSearch).index;
+                //                 var lengthOri = upperSearch.length;
+                //                 var oriText = $(this).text().substr(indexOri, lengthOri);
+                //                 const redRegExp = new RegExp(data.researchSave, 'gi');
     
-                                var ans = $(this).html().replace(redRegExp, "<span style=\"background-color:pink\">" + oriText + "</span>")
-                                // 문서번호 일경우
-                                if($(this).hasClass("doc_num")) {
-                                    $(this).find(".so_doc_no").html(ans);
-                                } else {
-                                    $(this).html(ans);
-                                }
-                            }
-                        });
+                //                 var ans = $(this).html().replace(redRegExp, "<span style=\"background-color:pink\">" + oriText + "</span>")
+                //                 // 문서번호 일경우
+                //                 if($(this).hasClass("doc_num")) {
+                //                     $(this).find(".so_doc_no").html(ans);
+                //                 } else {
+                //                     $(this).html(ans);
+                //                 }
+                //             }
+                //         });
 
-                    } else if (data.researchOption != "so_all" && data.researchSave) {
-                        $(".tblLatestList ." + data.researchOption).each(function() {
-                            var tdText = $(this).text().toUpperCase();
-                            var upperSearch = data.researchSave.toUpperCase();
-                            if(tdText.match(upperSearch)) {
-                                var indexOri = tdText.match(upperSearch).index;
-                                var lengthOri = upperSearch.length;
-                                var oriText = $(this).text().substr(indexOri, lengthOri);
-                                const redRegExp = new RegExp(data.researchSave, 'gi');
+                //     } else if (data.researchOption != "so_all" && data.researchSave) {
+                //         $(".tblLatestList ." + data.researchOption).each(function() {
+                //             var tdText = $(this).text().toUpperCase();
+                //             var upperSearch = data.researchSave.toUpperCase();
+                //             if(tdText.match(upperSearch)) {
+                //                 var indexOri = tdText.match(upperSearch).index;
+                //                 var lengthOri = upperSearch.length;
+                //                 var oriText = $(this).text().substr(indexOri, lengthOri);
+                //                 const redRegExp = new RegExp(data.researchSave, 'gi');
     
-                                var ans = $(this).text().replace(redRegExp, "<span style='background-color:pink'>" + oriText + "</span>")
-                                // 문서번호 일경우
-                                if($(this).hasClass("doc_num")) {
-                                    $(this).find(".so_doc_no").html(ans);
-                                } else {
-                                    $(this).html(ans);
-                                }
-                            }
-                        });
-                    }
-                } 
+                //                 var ans = $(this).text().replace(redRegExp, "<span style='background-color:pink'>" + oriText + "</span>")
+                //                 // 문서번호 일경우
+                //                 if($(this).hasClass("doc_num")) {
+                //                     $(this).find(".so_doc_no").html(ans);
+                //                 } else {
+                //                     $(this).html(ans);
+                //                 }
+                //             }
+                //         });
+                //     }
+                // } 
                 // else {
                 //     if(Object.keys(data.searchList).length > 0) {
                 //         for (var key in data.searchList) {
@@ -847,7 +850,7 @@ var vm = new Vue({
                                 <td class="responsiveTblRow so_tr" @click="docRowClick(doc.ms_no)">{{ doc.tr_doc_num }}</td>
                                 <td class="text-center responsiveTblRow" @click="docRowClick(doc.ms_no)">{{ doc.doc_distribute_date_str }}</td>
                                 <td class="text-center responsiveTblRow" @click="docRowClick(doc.ms_no)">{{ doc.doc_reply_date_str }}</td>
-                                <td class="text-center responsiveTblRow" @click="docRowClick(doc.ms_no)">{{ doc.doc_return_date_str }}</td>
+                                <td :class="['text-center', 'responsiveTblRow', {'beforeToday' : new Date(doc.doc_return_date_str) < new Date(doc.to_day_date_str)}]" @click="docRowClick(doc.ms_no)">{{ doc.doc_return_date_str }}</td>
                                 <td class="responsiveTblRow so_rt" @click="docRowClick(doc.ms_no)">{{ doc.doc_rfq_num }}</td>
                                 <td class="responsiveTblRow so_rt" @click="docRowClick(doc.ms_no)">{{ doc.doc_rfq_title }}</td>
                                 <td class="responsiveTblRow so_ti" @click="docRowClick(doc.ms_no)">{{ doc.doc_tag_item }}</td>
