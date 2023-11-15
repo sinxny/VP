abadefe24c8e6f643e0b484ca89b7ccbfdeb8938
sieun park <qkr990401@daum.net>
2023-11-15 16:01:07 +0900

    vp, cm 담당연락처 추가

--- www/welding/welding_day_view.php
+++ www/welding/welding_day_view.php
@@ -11,9 +11,9 @@
 .step0 {
     background-color:#F4B084;
 }
-.materialGrp {
-    /* background-color: #E2EFDA; */
-}
+/* .materialGrp {
+    background-color: #E2EFDA;
+} */
 .areaColor {
     /* background-color: #A9D08E; */
     text-align: center;
@@ -353,40 +353,11 @@ var vm = new Vue({
         </table>
     </div>
 </div>
-<div class="alert alert-success text-center" v-show="!jno">
-  <strong>PROJECT를 선택하세요.</strong>
-</div>
 <div class="alert alert-warning" v-show="noData && !init">
     <strong>조건에 맞는 결과가 없습니다.</strong>
 </div>
-<div id="modalLoading" class="modal modal-loading" data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog modal-sm">
-        <div class="modal-content">
-            <!-- <i class="fa fa-spinner fa-pulse fa-3x text-primary"></i> -->
-            <!-- <div id="percent" style="padding:1rem;color:white;display:none"></div> -->
-        </div>
-    </div>
-</div>
-<div class="dx-overlay-content dx-loadpanel-content dx-state-visible" style="width: 200px; height: 90px; z-index: 1501; left: 50%; top: 50%;" v-show="jno">
-    <div class="dx-loadpanel-content-wrapper">
-        <div class="dx-loadpanel-indicator dx-loadindicator dx-widget">
-            <div class="dx-loadindicator-wrapper">
-                <div class="dx-loadindicator-content">
-                    <div class="dx-loadindicator-icon">
-                        <div class="dx-loadindicator-segment dx-loadindicator-segment7"></div>
-                        <div class="dx-loadindicator-segment dx-loadindicator-segment6"></div>
-                        <div class="dx-loadindicator-segment dx-loadindicator-segment5"></div>
-                        <div class="dx-loadindicator-segment dx-loadindicator-segment4"></div>
-                        <div class="dx-loadindicator-segment dx-loadindicator-segment3"></div>
-                        <div class="dx-loadindicator-segment dx-loadindicator-segment2"></div>
-                        <div class="dx-loadindicator-segment dx-loadindicator-segment1"></div>
-                        <div class="dx-loadindicator-segment dx-loadindicator-segment0"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="dx-loadpanel-message">Loading...</div>
-    </div>
-</div>
 </form>
+<?php
+    require_once "welding_include.php";
+?>
 </div>
